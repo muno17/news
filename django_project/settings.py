fa6dc0a0dca270f4e3948405703f6d28e45505e2
Danny Muñoz <munozd17@gmail.com>
2026-05-10 16:10:09 -0400

    add articles app

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Local
     'accounts',
     'pages',
+    'articles'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
+USE_I18N = True
+USE_L10N = True
 
 USE_I18N = True
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
      path('', include('pages.urls')),
+    path('articles/', include('articles.urls')),
 
 ]
